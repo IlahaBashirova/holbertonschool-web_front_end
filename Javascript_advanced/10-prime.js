ed0542abb1d76a6c08c1c1b4a4f2588c349ea62e
IlahaBashirova <11861@holbertonstudents.com>
2025-10-18 14:34:13 +0400

    irjtng

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,22 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+const startTime = performance.now();
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+const endTime = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
