d70f08b0198d541cd77477c86dba0d41f6a175c8
IlahaBashirova <11861@holbertonstudents.com>
2025-10-20 01:31:50 +0400

    rijtgn

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -1,40 +1,37 @@
-class studentHogwarts {
-  #privateScore = 0;
-  #name = null;
-
-  #changeScoreBy(points) {
-    this.#privateScore += points;
-  }
-
-  setName(newName) {
-    this.#name = newName;
-  }
-
-  rewardStudent() {
-    this.#changeScoreBy(1);
-  }
-
-  penalizeStudent() {
-    this.#changeScoreBy(-1);
-  }
-
-  getScore() {
-    return `${this.#name}: ${this.#privateScore}`;
-  }
+class studentHogwarts{
+    constructor(privateScore, name) {
+        this.privateScore = 0;
+        this.name = null;
+    }
+    changeScoreBy(points) {
+        points += this.privateScore;
+    }
+    setName(newName) {
+        this.name = newName;
+        return this.name; 
+    }
+    rewardStudent() {
+        this.changeScoreBy(1);
+    }
+    penalizeStudent() {
+        this.changeScoreBy(-1);
+    }
+    getScore() {
+        return `${this.name}: ${this.privateScore}`;
+    }
 }
+let Harry = new studentHogwarts();
+Harry.setName("Harry");
+Harry.rewardStudent();
+Harry.rewardStudent();
+Harry.rewardStudent();
+Harry.rewardStudent();
+console.log(Harry.getScore());
 
-let harry = new studentHogwarts();
-harry.setName("Harry");
-harry.rewardStudent();
-harry.rewardStudent();
-harry.rewardStudent();
-harry.rewardStudent();
-console.log(harry.getScore());
-
-let draco = new studentHogwarts();
-draco.setName("Draco");
-draco.rewardStudent();
-draco.penalizeStudent();
-draco.penalizeStudent();
-draco.penalizeStudent();
-console.log(draco.getScore());
+let Draco = new studentHogwarts();
+Draco.setsetname("Draco");
+Draco.rewardStudent();
+Draco.penalizeStudent();
+Draco.penalizeStudent();
+Draco.penalizeStudent();
+console.log(Draco.getScore());
